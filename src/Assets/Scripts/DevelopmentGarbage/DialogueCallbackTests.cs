579635a133369319ee3453dec5fff65ef79cec6c
Georg Becker <georg.becker@shellrider.com>
2023-06-12 18:42:52 +0200

    Create linear dialogue system with callback functions

---
+++ src/Assets/Scripts/DevelopmentGarbage/DialogueCallbackTests.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueCallbackTests : MonoBehaviour
+{
+   public void LogFirst()
+   {
+      Debug.Log("First text is first");
+   }
+
+   public void LogSecond()
+   {
+      Debug.Log("Second text is second");
+   }
+}
