6c39a25b7d8b1df54c9c78cd1f86c435607b0968
Georg Becker <georg.becker@shellrider.com>
2023-06-12 13:16:27 +0200

    Start potion system

---
+++ src/Assets/Scripts/Dictionaries/IngredientDictionary.cs
@@ -0,0 +1,11 @@
+using JetBrains.Annotations;
+
+namespace Dictionaries
+{
+    [System.Serializable]
+    public struct IngredientDictionary
+    {
+        public int placeholder1;
+        public int placeholder2;
+    }
+}
