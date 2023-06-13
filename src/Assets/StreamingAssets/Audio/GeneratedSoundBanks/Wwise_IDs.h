84b321f45f4773c89a333ca333ebef8f454edcab
Annie Bonav <anniebonav@outlook.com>
2023-06-13 14:16:37 +0200

    Adds Wwise stuff

--- src/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ src/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,11 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_POP = 2990278359U;
+    } // namespace EVENTS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
